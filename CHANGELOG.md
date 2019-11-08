830f605615e1ed26bd2258968251de9dc3e358b4
CJ Wong <cj-wong@users.noreply.github.com>
2019-11-08 10:18:04 -0800

    Fix response error

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [1.1.2-cf] - 2019-11-08
+### Fixed
+- Fixed issue when *IPify* returns an application error. Added an error to raise.
+
 ## [1.1.1-cf] - 2019-11-07
 ### Fixed
 - Fixed missing argument in [`cloudflare.py`](cloudflare.py) `update_subdomain`
--- ipaddr.py
+++ ipaddr.py
@@ -1,4 +1,9 @@
+import re
+
 import requests
+
+
+IPADDR = re.compile('([0-9]+\.){3}[0-9]+')
 
 
 def get_current_ipaddr() -> str:
@@ -7,5 +12,26 @@ def get_current_ipaddr() -> str:
     Returns:
         str: the IP address
 
+    Raises:
+        APIResponseError: if an invalid response was detected
+
     """
-    return requests.get('https://api.ipify.org').text
+    response = requests.get('https://api.ipify.org').text
+    if IPADDR.match(response):
+        return response
+    else:
+        raise APIResponseError(response)
+
+
+class Error(Exception):
+    """Base error class for ipaddr.py"""
+    pass
+
+
+class APIResponseError(Error):
+    """The *ipify* API did not return a valid IP address."""
+    def __init__(self, response):
+        super().__init__(
+            'The *ipify* API did not return a valid IP address. '
+            f'Response: {response}'
+            )
